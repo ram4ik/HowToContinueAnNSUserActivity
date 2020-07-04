14d78d3baad27579829e150f65e17fcf9a3756e2
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-07-04 15:21:55 +0300

    Initial Commit

---
+++ HowToContinueAnNSUserActivity/HowToContinueAnNSUserActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToContinueAnNSUserActivityApp.swift
+//  HowToContinueAnNSUserActivity
+//
+//  Created by Ramill Ibragimov on 04.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct HowToContinueAnNSUserActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
